5dfe1e836464ae383716266da19ee82a2a1498b0
Nabeel Ali Memon <nabeel.memon@intigral.net>
2017-01-16 20:18:10 +0400

    Arrays chapter started.

---
+++ src/main/kotlin/com/ctci/chapter1/arrays.kt
@@ -0,0 +1,11 @@
+package com.ctci.chapter1
+
+// 1.1
+// check if a string has all unique characters
+
+
+// take a fixed boolean array of 256 (for ascii chars)
+fun allUnique0(str: String): Boolean {
+	val flags = BooleanArray(256) { false }
+	return false
+}
