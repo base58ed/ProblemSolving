2f6b9216f7a137ed0206f697efdfd1a119db9034
Nabeel Ali Memon <nabeel.memon@intigral.net>
2017-01-21 21:08:47 +0400

    Kotlin coroutines testing branch.

---
+++ src/main/kotlin/concurrent/generators/fibonacci.kt
@@ -0,0 +1,20 @@
+package concurrent.generators
+
+import sequence.buildSequence
+
+// inferred type is Sequence<Int>
+val fibonacci = buildSequence {
+	yield(1) // first Fibonacci number
+	var cur = 1
+	var next = 1
+	while (true) {
+		yield(next) // next Fibonacci number
+		val tmp = cur + next
+		cur = next
+		next = tmp
+	}
+}
+
+fun main(args: Array<String>) {
+	println(fibonacci.take(10).joinToString())
+}
